803b7dfe85cc3b8b09b04947e7056b4e869bc8c8
wangchuan <15935144396@163.com>
2023-07-22 13:00:16 +0800

    update deploye.sh

--- deploye.sh
+++ deploye.sh
@@ -1,27 +1,10 @@
 #!/bin/bash
 
-
 # Path to local JAR file
-LOCAL_JAR=build/libs/tbiops-0.0.1-SNAPSHOT.jar
+scp -r build/libs/tbiops-0.0.1-SNAPSHOT.jar root@10.60.1.206:~/
 
-# Remote server info
-REMOTE_USER=root
-REMOTE_HOST=10.60.1.206
-REMOTE_PATH=~/
 
-# Upload JAR file
-scp $LOCAL_JAR $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
-
-# SSH to remote server
-ssh $REMOTE_USER@$REMOTE_HOST << EOF
-
-# Kill any running process for this JAR
-kill -9 $(ps -ef | pgrep -f "tbiops-0.0.1-SNAPSHOT.jar")
-
-# Start the new JAR in the background
-nohup java -jar tbiops-0.0.1-SNAPSHOT.jar >java.log &
-
-EOF
+ssh  root@10.60.1.206 "sh ~/start.sh"
 
 
 
